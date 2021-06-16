c9def7a0e007bb212b336ffc109ee6312e27ba9b
kmorgport <kmorgan0057@gmail.com>
2021-06-16 16:56:04 -0500

    filtering routes

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 //REMEMBER, the order MATTERS
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
 app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = requires('express');
 const router = express.Router();
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
     //dont use next, you dont want to send more than one response or 
     //it will cause an error
 })
