1f4565d1fb9b22893bf2ef3ec0723e252c9bdb01
kmorgport <kmorgan0057@gmail.com>
2021-06-16 16:35:25 -0500

    parsing requests with express

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const http = require('http');
 const express = require('express');
-
+const bodyParser = require('body-parser')
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000)
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    //dont use next, you dont want to send more than one response or 
+    //it will cause an error
+})
+
+//get will only fire for incoming get requests
+//.post will only fire for incoming post requests
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+//<-- using express -->
+//next is a function
+app.use('/',(req, res, next)=>{
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000)
