6b6e56fd1a2d495c800eb8dd203e84076d9fbd1c
kmorgport <kmorgan0057@gmail.com>
2021-06-28 18:01:40 -0500

    refactoring models code

--- models/product.js
+++ models/product.js
@@ -3,39 +3,35 @@ const path = require('path')
 
 const products = [];
 
-module.exports = class Product {
-    constructor(title){
-        this.title = title
-    }
-
-    save(){
-        const p = path.join(path.dirname(process.mainModule.filename), 
-        'data',
-        'products.json'
-        );
-        fs.readFile(p, (err, fileContent)=>{
-            let products = [];
-            if (!err){
-                products =  JSON.parse(fileContent)
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products),(err)=>{
-                console.log(err)
-            })
-        });
-    }
-
-    static fetchAll(cb){
-        const p = path.join(
+const getProductsFromFile = cb => {
+    const p = path.join(
         path.dirname(process.mainModule.filename), 
         'data',
         'products.json'
         );
         fs.readFile(p, (err, fileContent)=>{
             if(err){
-                cb([])
+                return cb([])
             }
             cb(JSON.parse(fileContent))
         })
+}
+
+module.exports = class Product {
+    constructor(title){
+        this.title = title
+    }
+
+    save(){
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err =>{
+                console.log(err)
+            })
+        });
+    }
+
+    static fetchAll(cb){
+       getProductsFromFile(cb);
     }
 }
