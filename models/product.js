afb55d88cdb8c6598da7829149603902aac061d5
kmorgport <kmorgan0057@gmail.com>
2021-06-27 20:08:07 -0500

    saving files with node js

--- models/product.js
+++ models/product.js
@@ -9,11 +9,28 @@ module.exports = class Product {
     }
 
     save(){
-        const p = path.join(path.dirname(process.mainModule.filename))
-        products.push(this)
+        const p = path.join(path.dirname(process.mainModule.filename), 
+        'data',
+        'products.json'
+        );
+        fs.readFile(p, (err, fileContent)=>{
+            let products = [];
+            if (!err){
+                products =  JSON.parse(fileContent)
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err)=>{
+                console.log(err)
+            })
+        });
     }
 
     static fetchAll(){
-        return products;
+        fs.readFile(p, (err, fileContent)=>{
+            if(err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        })
     }
 }
