d5771f93480248a8803962fd8c3ecb62b367f46d
kmorgport <kmorgan0057@gmail.com>
2021-07-09 14:26:04 -0500

    sql database practice

--- models/cart.js
+++ models/cart.js
@@ -41,8 +41,11 @@ module.exports = class Cart {
         if(err) {
             return;
         }
-        const updatedCart = { ...fileContent};
+        const updatedCart = { ...JSON.parse(fileContent)};
         const product = updatedCart.products.find(prod => prod.id === id);
+        if(!product){
+            return;
+        }
         const productQty = product.qty;
         updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
         updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
